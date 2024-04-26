a3be1da2b970e5330ff19e5908a4ec9a7d40817b
Rafaela Perruci <rafaelaoperruci@gmail.com>
2024-04-26 16:47:50 -0300

    first commit - configurações

---
+++ src/main/java/br/com/rafaelaperruci/app/postssocialmediaworkshop/WorkshopPostsSocialmediaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rafaelaperruci.app.postssocialmediaworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopPostsSocialmediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopPostsSocialmediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rafaelaperruci/app/postssocialmediaworkshop/WorkshopPostsSocialmediaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rafaelaperruci.app.postssocialmediaworkshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopPostsSocialmediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
